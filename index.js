344645c6dd875cf480ca6b04e83967e4e972fcaf
soumahoro355 <soumahorochris355@gmail.com>
2026-07-09 18:35:44 +0000

    Premier test Railway

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-console.log("ok 1");
-app.use("/publication", require("./resource/Post"));
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 
+app.options("*", cors());
+
+app.use(express.json());
+
+console.log("SERVER STARTED");
+
+app.use("/publication", require("./resource/Post"));
+app.get("/", (req,res)=>{
+    console.log("ROOT OK");
+
+    res.json({
+        message:"API fonctionne"
+    });
+});
 app.listen(3000, () => {
     console.log("Serveur lancé");
 });
